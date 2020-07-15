d4898b4a258fc00cffe1739bc524ea167573fca2
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2020-07-15 11:19:53 -0500

    Update login.dart
    Trying to get the login.dart going

--- half_1.1/half/lib/pages/login.dart
+++ half_1.1/half/lib/pages/login.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
 import 'package:half/services/cloudFirestore.dart';
+import 'package:half/services/themes.dart';
+import 'package:half/services/dimensions.dart';
 import 'package:half/widgets/interfaceStandards.dart';
 import 'package:half/pages/home.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //Variable initialization
+  CloudFirestore cloudFirestore = new CloudFirestore();
+  InterfaceStandards interfaceStandards = new InterfaceStandards();
+  Themes themes = new Themes();
+  Dimensions dimensions = new Dimensions();
+
+  //User interface: Login screen
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Container(
+        child: Text(
+          "Testing",
+        ),
+      ),
+    );
+  }
+}
