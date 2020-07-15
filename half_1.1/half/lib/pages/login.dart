1fd7e8eba1ebb5fb53758e02af676d719fe73b23
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2020-07-15 11:16:07 -0500

    Update login.dart
    I need to figure out if Im going to do the route.dart FILE or just create some routes

---
+++ half_1.1/half/lib/pages/login.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+import 'package:half/services/cloudFirestore.dart';
+import 'package:half/widgets/interfaceStandards.dart';
+import 'package:half/pages/home.dart';
