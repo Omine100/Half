29405b8b7a3f764cda3ad6d987dc3d5ed6dee26e
Omine100 <54552984+Omine100@users.noreply.github.com>
2020-08-10 15:30:57 -0500

    Update login.dart
    Trying to get this thing set up.
    
    After this, I think that I have to do the connect screen, then I can test everything.
    
    If all of that works, then I can move onto home.dart and finish up the beta

--- half_1.3/half/lib/pages/login.dart
+++ half_1.3/half/lib/pages/login.dart
@@ -1,1 +1,46 @@
 import 'package:flutter/material.dart';
+
+import 'package:half/services/cloudFirestore.dart';
+import 'package:half/services/themes.dart';
+import 'package:half/widgets/interfaceStandards.dart';
+import 'package:half/pages/forgotPassword.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({this.loginCallback});
+
+  //Variable reference
+  final VoidCallback loginCallback;
+
+  @override
+  State<StatefulWidget> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //Variable initialization
+  CloudFirestore cloudFirestore = new CloudFirestore();
+  Themes themes = new Themes();
+  InterfaceStandards interfaceStandards = new InterfaceStandards();
+  final _formKey = GlobalKey<FormState>();
+  String _name, _email, _password, _errorMessage;
+  bool _isLoading, _isSignIn, _isVisible;
+
+  //Initial state
+  @override
+  void initState() {
+    super.initState();
+    _errorMessage = "";
+    _isSignIn = true;
+    _isLoading = false;
+    _isVisible = false;
+  }
+
+  //Mechanics: Validate and save user information
+  bool validateAndSave() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+}
