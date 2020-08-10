7bbf34fdcb6b4d3cf1f8a49e77375f7c603ee958
Omine100 <54552984+Omine100@users.noreply.github.com>
2020-08-10 14:51:31 -0500

    Update root.dart
    Trying to get this somewhat set up, so that I can really get into it when I start creating the pages

---
+++ half_1.3/half/lib/services/root.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/rendering.dart';
+
+import 'package:half/services/cloudFirestore.dart';
+import 'package:half/pages/login.dart';
+import 'package:half/pages/connector.dart';
+import 'package:half/pages/home.dart';
+
+//Status declaration
+enum AuthStatus {
+  NOT_DETERMINED,
+  NOT_SIGNED_IN,
+  SIGNED_IN,
+}
+
+class RootScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
+  //Variable initialization
+  CloudFirestore cloudFirestore = new CloudFirestore();
+  final db = Firestore.instance;
+  AuthStatus _authStatus = AuthStatus.NOT_DETERMINED;
+  String _userId = "", _partnerId = "", _partnerName = "";
+
+  //Initial state
+  @override
+  void initState() {
+    super.initState();
+    cloudFirestore.getCurrentUser().then((userId) {
+      setState(() {
+        if (userId != null) {
+          _userId = userId?.uid;
+        }
+        _authStatus = userId?.uid == null ? AuthStatus.NOT_SIGNED_IN : AuthStatus.SIGNED_IN;
+      });
+    });
+  }
+
+  //Mechanics: Sets status to logged in with current user
+  void loginCallback() {
+    cloudFirestore.getCurrentUserId().then((userId) {
+      cloudFirestore.getPartnerData().then((partnerId) {
+        cloudFirestore.getPartnerNameData().then((partnerName) {
+          setState(() {
+            userId != "" ? _userId = userId : _userId = "null";
+            partnerId != "" ? _partnerId = partnerId : _partnerId = "null";
+            partnerName != "" ? _partnerName = partnerName : _partnerName = "null";
+            print("Root page userId: " + _userId);
+            print("Root page partnerId: " + _partnerId);
+            print("Root page partnerName: " + _partnerName);
+          });
+        });
+      });
+    });
+    setState(() {
+      _authStatus = AuthStatus.SIGNED_IN;
+    });
+  }
+
+  //Mechanics: Sets status to logged out
+  void logoutCallback() {
+    setState(() {
+      _authStatus = AuthStatus.NOT_SIGNED_IN;
+      _userId = "";
+    });
+  }
+
+  //User interface: Builds circular progress indicator with animation
+  Widget buildWaitingScreen() {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  //User interface: Root screen
+  @override
+  Widget build(BuildContext context) {
+    switch (_authStatus) {
+      case AuthStatus.NOT_DETERMINED:
+        return buildWaitingScreen();
+        break;
+      case AuthStatus.NOT_SIGNED_IN:
+        return new LoginScreen(
+          loginCallback: loginCallback,
+        );
+        break;
+      case AuthStatus.SIGNED_IN:
+        if (_userId.length > 0 && _userId != null) {
+          if (cloudFirestore.getPartnerData() != null) {
+            return new HomeScreen(
+              logoutCallback: logoutCallback,
+              userId: _userId,
+              partnerId: _partnerId,
+              partnerName: _partnerName,
+            );
+          } else {
+            return new ConnectorScreen(
+              logoutCallback: logoutCallback,
+              userId: _userId,
+              partnerId: _partnerId,
+              partnerName: _partnerName,
+            );
+          }
+        } else
+          return buildWaitingScreen();
+        break;
+      default:
+        return buildWaitingScreen();
+    }
+  }
+}
