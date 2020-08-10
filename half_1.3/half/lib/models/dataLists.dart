8d78f4c6f38376c03dc83e5351379d13c2a0920d
Omine100 <54552984+Omine100@users.noreply.github.com>
2020-08-10 15:15:30 -0500

    Create dataLists.dart

---
+++ half_1.3/half/lib/models/dataLists.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DataLists {
+  //Mechanics: Get relationship data
+  String getRelationshipData(int position) {
+    List<String> _relationshipList = getRelationshipList();
+    return _relationshipList[position];
+  }
+
+  //Mechanics: Get relationship list
+  List<String> getRelationshipList() {
+    List<String> _relationshipList = new List<String>();
+    _relationshipList.add("Single");
+    _relationshipList.add("Talking");
+    _relationshipList.add("Dating");
+    _relationshipList.add("Married");
+    return _relationshipList;
+  }
+}
