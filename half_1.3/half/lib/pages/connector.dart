277422159795f1cee3d9fe46ddc4f8484287c8bb
Omine100 <54552984+Omine100@users.noreply.github.com>
2020-08-15 11:40:05 -0500

    Update connector.dart

--- half_1.3/half/lib/pages/connector.dart
+++ half_1.3/half/lib/pages/connector.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:half/services/cloudFirestore.dart';
 import 'package:half/services/themes.dart';
 import 'package:half/widgets/interfaceStandards.dart';
-import 'package:half/pages/home.dart';
 
 class ConnectorScreen extends StatefulWidget {
   ConnectorScreen({Key key, this.signInCallback, this.userId});
